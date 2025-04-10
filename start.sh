5f919173c9f17809118ac06c7ac08dc451670b0f
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-04-10 11:59:46 +0000

    updated the port

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 mkdir -p /root/.kaggle
 cp kaggle.json /root/.kaggle/kaggle.json
 chmod 600 /root/.kaggle/kaggle.json
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+streamlit run app.py --server.port 80 --server.address 0.0.0.0
